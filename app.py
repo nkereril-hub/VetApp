10e88c6b708d33f6a82de9b570f18aea6aebb103
nkereril-hub <nkereril@gmail.com>
2026-03-19 21:14:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -189,8 +189,10 @@ def register_treatment():
 def logout():
     session.clear()
     return redirect(url_for('login'))
+with app.app_context(): 
+    init_db()
 
 if __name__ == '__main__':
-    with app.app_context(): init_db()
+   
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
